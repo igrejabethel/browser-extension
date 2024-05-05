8685c745ec368f673920965f09e16868b9cf818c
João Paulo Borges e Silva <joaopaulo@hayato.dev.br>
2024-05-05 10:15:03 -0300

    Update Chord Remover

--- Cifra Club - Chord Remover/chord-remover.js
+++ Cifra Club - Chord Remover/chord-remover.js
@@ -1,13 +1,21 @@
-$(document).ready(function() {
-    $('pre b').remove()
+$(document).ready(function () {
+	$('pre b').remove()
 })
 
-$(document).ready(function() {
-    $('pre').each(function() {
-        var preContent = $(this).html()
+$(document).ready(function () {
+	$('pre').each(function () {
+		var preContent = $(this).html()
 
-        var newContent = preContent.replace('\n ', '\n')
+		var newContent = preContent
+			.replace('\n ', '\n')
+			.replace('[Intro]', '')
+			.replace('[Final]', '')
 
-        $(this).html(newContent)
-    })
+		$(this).html(newContent)
+
+		$('#cifra_capo').append(
+			`<h2>Lyric</h2>
+<textarea style="width: 500px; height: 400px">${newContent}</textarea>`
+		)
+	})
 })
